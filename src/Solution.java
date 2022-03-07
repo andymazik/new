e06cbc3675c5bd7c23b92ea59d29a7f8c2ef1ec5
”Andrey <andymazik@gmail.com>
2022-03-07 12:09:54 +0300

    My 1st newbranch2 commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,9 @@
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Java");
+        System.out.println("C++");
+        System.out.println("C++");
+        System.out.println("C++");
 
     }
 }
