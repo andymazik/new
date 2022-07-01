194aea8ee00ddf27dc52e1f513666d0e6df271b7
Andrey Mazikov <andymazik@gmail.com>
2022-07-01 20:48:14 +0300

    2nd commit

--- src/Solution.java
+++ src/Solution.java
@@ -3,5 +3,7 @@ public class Solution {
         System.out.println("Hello");
         System.out.println("I am robot");
         System.out.println("My name is Robert");
+
+        System.out.println("This is happy day");
     }
 }
